8ddba03b48e103a13cd9a1b8b3cd56338418fe52
EC2 Default User <ec2-user@ip-172-31-56-192.ec2.internal>
2021-06-15 01:09:13 +0000

    app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,51 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
-    # インスタンス変数bookを作成　Bookモデルに.newをつけることで　空のモデルを作成し→インスタンス変数に代入し→viewで扱えるようになる
+    @books = Book.all
+    # インスタンス変数bookを作成.newをつけることでこの画面で空のモデルを作成→インスタンス変数に代入し→viewで扱えるようになる
+    # つまりこのコントローラ＋アクションに紐付いているviewで使用するだけのパーツなのでindexアクションに色々追加しても大丈夫なはず
+    #（投稿機能を追加しようとしている）
   end
 
   def create
-    # １. データを新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/books'
+    @books = Book.all
+    @book = Book.new(book_params)
+    # 1. データをデータベースに保存するためのsaveメソッド実行
+   if @book.save
+     redirect_to book_show_path(book.id)
+   else
+     render:index
+   end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    # ３. 詳細画面へリダイレクト
+   redirect_to book_index_path
+  end
+
+  #詳細画面
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
+  #編集画面
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
+    if book.update(book_params)
+        redirect_to book_update_path(book.id)
+    else
+        render:edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get 'books' =>'books#index'
+ get 'books' =>'books#index',as:'book_index''
  post 'books' => 'books#create'
- get 'books/show' =>'books#show'
+ get 'books/:id' =>'books#show',as:'book_show'
+ get 'books/:id/edit' =>'books#edit',as:'book_edit'
+ patch 'books/:id' =>'books#update'
+ delete 'books/:id' => 'books#destroy',as:'book_destory'
  get 'books/new' =>'books#new'
  get 'books/edit' =>'books#edit'
- 
+
  root :to => 'homes#top'
+
 end
